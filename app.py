e15d23bcf4ef0b71f17faec67cbbbb62940207e9
S7575 <81707738+S7575@users.noreply.github.com>
2023-07-28 00:06:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit as st
 
 # Zahnnummern
 teeth_numbers = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
@@ -8,15 +9,17 @@ df = pd.DataFrame(index=teeth_numbers, columns=['B', 'R', 'TP'])
 
 # Aufforderung zur Benutzereingabe für jede Zahnnummer
 for tooth in teeth_numbers:
-    print(f"Bitte geben Sie die Informationen für Zahnnummer {tooth} ein:")
-    b = input("Geben Sie den Wert für B ein: ")
-    r = input("Geben Sie den Wert für R ein: ")
-    tp = input("Geben Sie den Wert für TP ein: ")
+    st.subheader(f"Bitte geben Sie die Informationen für Zahnnummer {tooth} ein:")
+    b = st.text_input(f"Geben Sie den Wert für B ein für Zahnnummer {tooth}: ")
+    r = st.text_input(f"Geben Sie den Wert für R ein für Zahnnummer {tooth}: ")
+    tp = st.text_input(f"Geben Sie den Wert für TP ein für Zahnnummer {tooth}: ")
 
     # Speichern Sie die Werte im DataFrame
     df.loc[tooth, 'B'] = b
     df.loc[tooth, 'R'] = r
     df.loc[tooth, 'TP'] = tp
 
-# Ausgabe des DataFrame in CSV-Format
-print(df.to_csv(sep=';'))
+# Eine Schaltfläche, um die Eingabe zu steuern und die CSV-Tabelle auszugeben
+if st.button("Erstellen Sie die CSV-Tabelle"):
+    # Ausgabe des DataFrame in CSV-Format
+    st.write(df.to_csv(sep=';'))
