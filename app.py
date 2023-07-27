fc4170ef48237cbe2e0c0009ae64a906f62170c5
S7575 <81707738+S7575@users.noreply.github.com>
2023-07-28 00:15:06 +0200

    neu

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid
 
 # Zahnnummern
 teeth_numbers = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
@@ -7,19 +8,9 @@ teeth_numbers = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
 # Initialisieren Sie ein leeres DataFrame
 df = pd.DataFrame(index=teeth_numbers, columns=['B', 'R', 'TP'])
 
-# Aufforderung zur Benutzereingabe für jede Zahnnummer
-for tooth in teeth_numbers:
-    st.subheader(f"Bitte geben Sie die Informationen für Zahnnummer {tooth} ein:")
-    b = st.text_input(f"Geben Sie den Wert für B ein für Zahnnummer {tooth}: ")
-    r = st.text_input(f"Geben Sie den Wert für R ein für Zahnnummer {tooth}: ")
-    tp = st.text_input(f"Geben Sie den Wert für TP ein für Zahnnummer {tooth}: ")
+# Legen Sie das DataFrame zurück
+df.reset_index(inplace=True)
+df = df.rename(columns={'index': 'Zähne'})
 
-    # Speichern Sie die Werte im DataFrame
-    df.loc[tooth, 'B'] = b
-    df.loc[tooth, 'R'] = r
-    df.loc[tooth, 'TP'] = tp
-
-# Eine Schaltfläche, um die Eingabe zu steuern und die CSV-Tabelle auszugeben
-if st.button("Erstellen Sie die CSV-Tabelle"):
-    # Ausgabe des DataFrame in CSV-Format
-    st.write(df.to_csv(sep=';'))
+# Erstellen Sie das Grid
+AgGrid(df)
