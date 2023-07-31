c8eacafdd4e47f15c2bbecab7602ee8670b2ddc5
S7575 <81707738+S7575@users.noreply.github.com>
2023-07-31 18:22:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 # Zahnnummern
 teeth_numbers = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
@@ -12,5 +12,15 @@ df = pd.DataFrame(index=teeth_numbers, columns=['B', 'R', 'TP'])
 df.reset_index(inplace=True)
 df = df.rename(columns={'index': 'Zähne'})
 
+# Konfigurieren Sie das Gitter
+gb = GridOptionsBuilder()
+gb.set_default_editable(True)  # Stellen Sie dies auf 'True' um alle Zellen bearbeitbar zu machen
+gb.set_column_def("B", editable=True)  # Dies ermöglicht die Bearbeitung speziell für die Spalte 'B'
+gridOptions = gb.build()
+
 # Erstellen Sie das Grid
-AgGrid(df)
+response = AgGrid(df, gridOptions=gridOptions)
+
+# Der aktualisierte DataFrame wird in response['data'] gespeichert.
+df = response['data']
+
