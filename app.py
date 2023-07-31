4d85f420290899600ea6927bf1ed5ac75634d810
S7575 <81707738+S7575@users.noreply.github.com>
2023-07-31 18:25:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,16 @@
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid
 
 # Zahnnummern
 teeth_numbers = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
 
-# Initialisieren Sie ein leeres DataFrame
-df = pd.DataFrame(index=teeth_numbers, columns=['B', 'R', 'TP'])
+# Initialisieren Sie ein DataFrame mit Ausgangswerten
+df = pd.DataFrame(index=teeth_numbers, columns=['B', 'R', 'TP']).fillna('')
 
 # Legen Sie das DataFrame zurück
 df.reset_index(inplace=True)
 df = df.rename(columns={'index': 'Zähne'})
 
-# Konfigurieren Sie das Gitter
-gb = GridOptionsBuilder()
-gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, editable=True)
-gridOptions = gb.build()
-
 # Erstellen Sie das Grid
-response = AgGrid(df, gridOptions=gridOptions, editable=True, height=600)
-
-# Der aktualisierte DataFrame wird in response['data'] gespeichert.
-df = response['data']
+AgGrid(df, editable=True, height=600)
