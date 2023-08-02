5db70b9807bd86892ab56241f14968c9effbc843
S7575 <81707738+S7575@users.noreply.github.com>
2023-08-02 22:58:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from st_aggrid import AgGrid
 import pandas as pd
 
 # Define teeth and dropdown options
-teeth = [18,17,16,15,14,13,12,11,21,22,23,24,25,26,27,28]
+teeth1 = [18,17,16,15,14,13,12,11]
+teeth2 = [21,22,23,24,25,26,27,28]
 options = ['ww', 'x']
 
-# Initialize DataFrame
-df = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth])
-df = df.fillna('')
+# Initialize DataFrames
+df1 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth1])
+df1 = df1.fillna('')
+
+df2 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth2])
+df2 = df2.fillna('')
 
 # Configure grid options
 grid_options = {
@@ -23,17 +27,26 @@ grid_options = {
             'cellEditorParams': {
                 'values': options
             }
-        } for tooth in teeth
+        } for tooth in teeth1 + teeth2
     ]
 }
 
-
-
-# Create AgGrid
-response = AgGrid(
-    df.reset_index().rename(columns={'index':' '}),
+# Create AgGrids
+response1 = AgGrid(
+    df1.reset_index().rename(columns={'index':' '}),
     gridOptions=grid_options,
-    height=175,
+    height=200,
+    width='100%',
+    data_return_mode='as_input',
+    update_mode='value_changed',
+    fit_columns_on_grid_load=True,
+    allow_unsafe_js_code=True,
+)
+
+response2 = AgGrid(
+    df2.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options,
+    height=200,
     width='100%',
     data_return_mode='as_input',
     update_mode='value_changed',
@@ -43,15 +56,16 @@ response = AgGrid(
 
 # If the grid's data has been updated...
 if st.button('Update Tabel'):
-    if response['data'] is not None:
-        updated_df = response['data'].set_index(' ')
-        for tooth in teeth:
-            if updated_df.loc['B', str(tooth)] == 'ww':
-                updated_df.loc['R', str(tooth)] = 'KV'
-                updated_df.loc['TP', str(tooth)] = 'KV'
-            elif updated_df.loc['B', str(tooth)] == 'x':
-                updated_df.loc['R', str(tooth)] = 'E'
-                updated_df.loc['TP', str(tooth)] = 'E'
+    for response, teeth in [(response1, teeth1), (response2, teeth2)]:
+        if response['data'] is not None:
+            updated_df = response['data'].set_index(' ')
+            for tooth in teeth:
+                if updated_df.loc['B', str(tooth)] == 'ww':
+                    updated_df.loc['R', str(tooth)] = 'KV'
+                    updated_df.loc['TP', str(tooth)] = 'KV'
+                elif updated_df.loc['B', str(tooth)] == 'x':
+                    updated_df.loc['R', str(tooth)] = 'E'
+                    updated_df.loc['TP', str(tooth)] = 'E'
 
-        # Display final table with AgGrid
-        AgGrid(updated_df.reset_index().rename(columns={'index':' '}), editable=False, fit_columns_on_grid_load=True)
+            # Display final tables with AgGrid
+            AgGrid(updated_df.reset_index().rename(columns={'index':' '}), editable=False, fit_columns_on_grid_load=True)
