c63e8921e0eb260d33acd19c88f78bcef1851622
S7575 <81707738+S7575@users.noreply.github.com>
2023-08-02 23:04:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,84 +4,80 @@ import pandas as pd
 
 # Define teeth and dropdown options
 teeth1 = [18,17,16,15,14,13,12,11]
-teeth2 = [28,27,26,25,24,23,22,21]
+teeth2 = [21,22,23,24,25,26,27,28]
 options = ['ww', 'x']
 
-# Initialize DataFrames
+# Initialize DataFrame
 df1 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth1])
-df1 = df1.fillna('')
-
 df2 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth2])
+df1 = df1.fillna('')
 df2 = df2.fillna('')
 
-# Configure grid options for both dataframes
-grid_options1 = {
+# Configure grid options
+grid_options = {
     'defaultColDef': {
         'editable': True,
         'resizable': True,
+        'headerComponentParams': {'menuTabs': ['generalTab']},
     },
     'columnDefs': [
+        {
+            'headerName': ' ',
+            'field': ' ',
+            'width': 100,
+        }
+    ] + [
         {
             'field': str(tooth),
             'cellEditor': 'agSelectCellEditor',
             'cellEditorParams': {
                 'values': options
             }
-        } for tooth in teeth1
+        } for tooth in teeth1+teeth2
     ]
 }
 
-grid_options2 = {
-    'defaultColDef': {
-        'editable': True,
-        'resizable': True,
-    },
-    'columnDefs': [
-        {
-            'field': str(tooth),
-            'cellEditor': 'agSelectCellEditor',
-            'cellEditorParams': {
-                'values': options
-            }
-        } for tooth in teeth2
-    ]
-}
-
-# Create AgGrids
+# Create AgGrid
 response1 = AgGrid(
-     df1.reset_index().rename(columns={'index':'Indexname'}),
-    gridOptions=grid_options1,
-    height=150,
-    width='100%',
+    df1.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options,
+    height=300,
+    width='50%',
     data_return_mode='as_input',
     update_mode='value_changed',
     fit_columns_on_grid_load=True,
-    allow_unsafe_js_code=True,
+    allow_unsafe_js_code=True,  # This is required to enable onCellValueChanged callback
 )
 
 response2 = AgGrid(
-    df2.reset_index().rename(columns={'index':'Indexname'}),
-    gridOptions=grid_options2,
-    height=150,
-    width='100%',
+    df2.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options,
+    height=300,
+    width='50%',
     data_return_mode='as_input',
     update_mode='value_changed',
     fit_columns_on_grid_load=True,
-    allow_unsafe_js_code=True,
+    allow_unsafe_js_code=True,  # This is required to enable onCellValueChanged callback
 )
 
 # If the grid's data has been updated...
 if st.button('Update Tabel'):
-    for response, teeth in [(response1, teeth1), (response2, teeth2)]:
-        if response['data'] is not None:
-            updated_df = response['data'].set_index(' ')
-            for tooth in teeth:
-                if updated_df.loc['B', str(tooth)] == 'ww':
-                    updated_df.loc['R', str(tooth)] = 'KV'
-                    updated_df.loc['TP', str(tooth)] = 'KV'
-                elif updated_df.loc['B', str(tooth)] == 'x':
-                    updated_df.loc['R', str(tooth)] = 'E'
-                    updated_df.loc['TP', str(tooth)] = 'E'
+    if response1['data'] is not None and response2['data'] is not None:
+        updated_df1 = response1['data'].set_index(' ')
+        updated_df2 = response2['data'].set_index(' ')
+        for tooth in teeth1+teeth2:
+            if str(tooth) in updated_df1.columns:
+                df = updated_df1
+            else:
+                df = updated_df2
 
-            # Display final tables with AgGrid
-            AgGrid(updated_df.reset_index().rename(columns={'index':' '}), editable=False, fit_columns_on_grid_load=True)
+            if df.loc['B', str(tooth)] == 'ww':
+                df.loc['R', str(tooth)] = 'KV'
+                df.loc['TP', str(tooth)] = 'KV'
+            elif df.loc['B', str(tooth)] == 'x':
+                df.loc['R', str(tooth)] = 'E'
+                df.loc['TP', str(tooth)] = 'E'
+
+        # Display final table with AgGrid
+        AgGrid(updated_df1.reset_index().rename(columns={'index':' '}), editable=False, fit_columns_on_grid_load=True)
+        AgGrid(updated_df2.reset_index().rename(columns={'index':' '}), editable=False, fit_columns_on_grid_load=True)
