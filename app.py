74c711cc1a9251a6fddde14b400f945ab97c81a7
S7575 <81707738+S7575@users.noreply.github.com>
2023-08-03 23:50:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -120,10 +120,12 @@ def update_values(df, teeth_set):
     for col in df.columns:
         if col != 'B':
             B_value = df.loc['B', col]
+            print(f"B_value for column {col}: {B_value}")  # Debugging statement
             if (B_value == 'ww').any():  # Use .any() to check if any elements in the Series are 'ww'
                 df.loc['R', col] = 'KV'
                 df.loc['TP', col] = 'KV'
     return df
+
 
 
 # Check if the button is pressed
