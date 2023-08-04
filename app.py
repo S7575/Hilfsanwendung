0a3d57cca0df07b31d903011ba0115d5462b4458
S7575 <81707738+S7575@users.noreply.github.com>
2023-08-04 09:56:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,221 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid
 import pandas as pd
 
-# Definieren der Tabellen
-data_1 = pd.DataFrame({'Zähne': [11,12,13,14,15,16,17,18], 'B': ['']*8, 'R': ['']*8, 'TP': ['']*8})
-data_2 = pd.DataFrame({'Zähne': [21,22,23,24,25,26,27,28], 'B': ['']*8, 'R': ['']*8, 'TP': ['']*8})
-data_3 = pd.DataFrame({'Zähne': [31,32,33,34,35,36,37,38], 'B': ['']*8, 'R': ['']*8, 'TP': ['']*8})
-data_4 = pd.DataFrame({'Zähne': [41,42,43,44,45,46,47,48], 'B': ['']*8, 'R': ['']*8, 'TP': ['']*8})
+# Define teeth and dropdown options
+teeth1 = [11,12,13,14,15,16,17,18]
+teeth2 = [21,22,23,24,25,26,27,28]
+teeth3 = [31,32,33,34,35,36,37,38]
+teeth4 = [41,42,43,44,45,46,47,48]
 
-datasets = [data_1, data_2, data_3, data_4]
+options = ['ww', 'x', 'a', 'ab', 'abw', 'aw', 'b', 'bw', 'e', 'ew', 'f', 'ix', 'k', 'kw', 'pkw', 'pw', 'r', 'rW', 'sb', 'sbw', 'se', 'sew', 'sk', 'skw', 'so', 'sow', 'st', 'stw', 't', 't2w', 'tw', 'ur', ')(']
 
-# Dropdown Optionen
-b_options = ['ww', 'x', 'a', 'ab', 'abw', 'aw', 'b', 'bw', 'e', 'ew', 'f', 'ix', 'k', 'kw', 'pkw', 'pw', 'r', 'rW', 'sb', 'sbw', 'se', 'sew', 'sk', 'skw', 'so', 'sow', 'st', 'stw', 't', 't2w', 'tw', 'ur', ')(']
-tp_options = ["A", "ABV", "ABM", "B", "BM", "BV", "E", "EO", "H", "K", "KH", "KM", "KMH"]
+# Initialize DataFrame
+df1 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth1])
+df2 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth2])
+df3 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth3])
+df4 = pd.DataFrame(index=['B', 'R', 'TP'], columns=[str(tooth) for tooth in teeth4])
+df1 = df1.fillna('')
+df2 = df2.fillna('')
+df3 = df3.fillna('')
+df4 = df4.fillna('')
 
-# Erzeugen der Check-Kästchen
-checkboxes = [
-    st.checkbox('1. Quadrat einblenden'),
-    st.checkbox('2. Quadrat einblenden'),
-    st.checkbox('3. Quadrat einblenden'),
-    st.checkbox('4. Quadrat einblenden'),
+# Create grid options for each table
+grid_options1 = {
+    'defaultColDef': {
+        'editable': True,
+        'resizable': True,
+        'headerComponentParams': {'menuTabs': ['generalTab']},
+    },
+    'columnDefs': [
+        {
+            'headerName': ' ',
+            'field': ' ',
+            'width': 100,
+        }
+    ] + [
+        {
+            'field': str(tooth),
+            'cellEditor': 'agSelectCellEditor',
+            'cellEditorParams': {
+                'values': options
+            }
+        } for tooth in teeth1
+    ]
+}
+
+grid_options2 = grid_options1.copy()
+grid_options2['columnDefs'] = [
+    {
+        'headerName': ' ',
+        'field': ' ',
+        'width': 100,
+    }
+] + [
+    {
+        'field': str(tooth),
+        'cellEditor': 'agSelectCellEditor',
+        'cellEditorParams': {
+            'values': options
+        }
+    } for tooth in teeth2
 ]
 
-def build_grid(data, options, tp_options):
-    # Konfigurieren der Spalten
-    gb = GridOptionsBuilder.from_dataframe(data)
-    gb.configure_column("B", cellEditor="agSelectCellEditor", cellEditorParams={"values": options}, editable=True)
-    gb.configure_column("TP", cellEditor="agSelectCellEditor", cellEditorParams={"values": tp_options}, editable=True)
-    gb.configure_grid_options(domLayout='autoHeight')
-    gridOptions = gb.build()
+grid_options3 = grid_options1.copy()
+grid_options3['columnDefs'] = [
+    {
+        'headerName': ' ',
+        'field': ' ',
+        'width': 100,
+    }
+] + [
+    {
+        'field': str(tooth),
+        'cellEditor': 'agSelectCellEditor',
+        'cellEditorParams': {
+            'values': options
+        }
+    } for tooth in teeth3
+]
 
-    # Ag-Grid anzeigen
-    grid_response = AgGrid(
-        data,
-        gridOptions=gridOptions,
-        height=200,
-        width='100%',
-        data_return_mode='as_input',  # Updates werden beim Editieren automatisch in den Dataframe übernommen
-        fit_columns_on_grid_load=True,
-        allow_unsafe_jscode=True,     # Erlaubt das Ausführen von Javascript Code
-    )
-    
-    return grid_response['data']
+grid_options4 = grid_options1.copy()
+grid_options4['columnDefs'] = [
+    {
+        'headerName': ' ',
+        'field': ' ',
+        'width': 100,
+    }
+] + [
+    {
+        'field': str(tooth),
+        'cellEditor': 'agSelectCellEditor',
+        'cellEditorParams': {
+            'values': options
+        }
+    } for tooth in teeth4
+]
 
-# Wenn der Session State noch nicht initialisiert wurde
-if "datasets" not in st.session_state:
-    st.session_state["datasets"] = datasets
 
-# Anzeigen der Tabellen, wenn die Kästchen ausgewählt sind
-for i in range(4):
-    if checkboxes[i]:
-        st.session_state.datasets[i] = build_grid(st.session_state.datasets[i], b_options, tp_options)
 
+# Create AgGrid
+response1 = AgGrid(
+    df1.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options1,
+    height=150,
+    width='50%',
+    data_return_mode='as_input',
+    update_mode='value_changed',
+    fit_columns_on_grid_load=True,
+    allow_unsafe_js_code=True,  # This is required to enable onCellValueChanged callback
+)
+
+response2 = AgGrid(
+    df2.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options2,
+    height=150,
+    width='50%',
+    data_return_mode='as_input',
+    update_mode='value_changed',
+    fit_columns_on_grid_load=True,
+    allow_unsafe_js_code=True,  # This is required to enable onCellValueChanged callback
+)
+
+response3 = AgGrid(
+    df3.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options3,
+    height=150,
+    width='50%',
+    data_return_mode='as_input',
+    update_mode='value_changed',
+    fit_columns_on_grid_load=True,
+    allow_unsafe_js_code=True,  # This is required to enable onCellValueChanged callback
+)
+
+response4 = AgGrid(
+    df4.reset_index().rename(columns={'index':' '}),
+    gridOptions=grid_options4,
+    height=150,
+    width='50%',
+    data_return_mode='as_input',
+    update_mode='value_changed',
+    fit_columns_on_grid_load=True,
+    allow_unsafe_js_code=True,  # This is required to enable onCellValueChanged callback
+)
+
+# If the grid's data has been updated...
 if st.button('Befund aktualisieren'):
-    for i in range(4):
-        if checkboxes[i]:
-            data = st.session_state.datasets[i].copy()  # copy of the dataframe
-            for index, row in data.iterrows():
-                if row['B'] == 'ww':
-                    if (row['Zähne'] in range(11, 19)) or (row['Zähne'] in range(21, 29)):
-                        if row['Zähne'] in [15, 16, 25, 26]:
-                            data.at[index, 'R'] = 'K'
-                        else:
-                            data.at[index, 'R'] = 'KV'
-                    elif (row['Zähne'] in range(31, 39)) or (row['Zähne'] in range(41, 49)):
-                        if row['Zähne'] in [35, 36, 45, 46]:
-                            data.at[index, 'R'] = 'K'
-                        else:
-                            data.at[index, 'R'] = 'KV'
-                elif row['B'] == 'x':
-                    data.at[index, 'R'] = 'E'
-            st.session_state["datasets"][i] = data  # Speichern der aktualisierten Daten
-            AgGrid(st.session_state["datasets"][i])  # Tabelle neu anzeigen
+    if response1['data'] is not None:
+        updated_df1 = response1['data'].set_index(' ')
+        # Check if there is any input in 'B' row
+        if not updated_df1.loc['B'].eq('').all():  # if all are empty strings, we assume there's no input
+            for tooth in teeth1:
+                if updated_df1.loc['B', str(tooth)] == 'ww':
+                    updated_df1.loc['R', str(tooth)] = 'KV'
+                    updated_df1.loc['TP', str(tooth)] = 'KV'
+                elif updated_df1.loc['B', str(tooth)] == 'x':
+                    updated_df1.loc['R', str(tooth)] = 'E'
+                    updated_df1.loc['TP', str(tooth)] = 'E'
+
+            # Display final table with AgGrid
+            AgGrid(updated_df1.reset_index().rename(columns={'index':' '}), 
+                   editable=False, 
+                   fit_columns_on_grid_load=True, 
+                   height=150, 
+                   key='AgGrid1')
+
+    if response2['data'] is not None:
+        updated_df2 = response2['data'].set_index(' ')
+        # Check if there is any input in 'B' row
+        if not updated_df2.loc['B'].eq('').all():  # if all are empty strings, we assume there's no input
+            for tooth in teeth2:
+                if updated_df2.loc['B', str(tooth)] == 'ww':
+                    updated_df2.loc['R', str(tooth)] = 'KV'
+                    updated_df2.loc['TP', str(tooth)] = 'KV'
+                elif updated_df2.loc['B', str(tooth)] == 'x':
+                    updated_df2.loc['R', str(tooth)] = 'E'
+                    updated_df2.loc['TP', str(tooth)] = 'E'
+
+            # Display final table with AgGrid
+            AgGrid(updated_df2.reset_index().rename(columns={'index':' '}), 
+                   editable=False, 
+                   fit_columns_on_grid_load=True, 
+                   height=150, 
+                   key='AgGrid2')
+            
+    if response3['data'] is not None:
+        updated_df3 = response3['data'].set_index(' ')
+        if not updated_df3.loc['B'].eq('').all():
+            for tooth in teeth3:
+                if updated_df3.loc['B', str(tooth)] == 'ww':
+                    updated_df3.loc['R', str(tooth)] = 'KV'
+                    updated_df3.loc['TP', str(tooth)] = 'KV'
+                elif updated_df3.loc['B', str(tooth)] == 'x':
+                    updated_df3.loc['R', str(tooth)] = 'E'
+                    updated_df3.loc['TP', str(tooth)] = 'E'
+
+            # Display final table with AgGrid
+            AgGrid(updated_df3.reset_index().rename(columns={'index':' '}), 
+                   editable=False, 
+                   fit_columns_on_grid_load=True, 
+                   height=150, 
+                   key='AgGrid3')
+            
+
+    if response4['data'] is not None:
+        updated_df4 = response4['data'].set_index(' ')
+        if not updated_df4.loc['B'].eq('').all():
+            for tooth in teeth4:
+                if updated_df4.loc['B', str(tooth)] == 'ww':
+                    updated_df4.loc['R', str(tooth)] = 'KV'
+                    updated_df4.loc['TP', str(tooth)] = 'KV'
+                elif updated_df4.loc['B', str(tooth)] == 'x':
+                    updated_df4.loc['R', str(tooth)] = 'E'
+                    updated_df4.loc['TP', str(tooth)] = 'E'
+
+            # Display final table with AgGrid
+            AgGrid(updated_df4.reset_index().rename(columns={'index':' '}), 
+                   editable=False, 
+                   fit_columns_on_grid_load=True, 
+                   height=150, 
+                   key='AgGrid4')
+
